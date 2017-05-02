ec5987255e8d6dda52ae9180c77ae33a97a3b05b
Mihai Blidaru <mihaiblidaru14@gmail.com>
2017-05-02 23:19:52 +0200

    Comentarios finales en todos los archivos

--- MicroMIPS.vhd
+++ MicroMIPS.vhd
@@ -190,137 +190,139 @@ begin
 
     -- Instancia de la unidad de control
     Inst_UnidadControl: UnidadControl PORT MAP(
-        OPCode     => MemProgData(31 downto 26),
-        Funct      => MemProgData(5  downto 0),
-        ALUControl => ALUControlCtrl,
-        MemToReg   => MemToRegCtrl,
-        MemWrite   => MemDataWE,
-        Branch     => BranchCtrl,
-        ALUSrc     => ALUSrcCtrl,
-        RegDest    => RegDestCtrl,
-        RegWrite   => RegWriteCtrl,
-        RegToPC    => RegToPCCtrl,
-        ExtCero    => ExtCeroCtrl,
-        Jump       => JumpCtrl,
-        PCToReg    => PCToRegCtrl
+        OPCode     => MemProgData(31 downto 26), -- Codigo de operacion
+        Funct      => MemProgData(5  downto 0),  -- Codigo de funcion
+        ALUControl => ALUControlCtrl,            -- Señal de control de la ALU
+        MemToReg   => MemToRegCtrl,              -- Se activa cuando se lleva un dato de memoria a un registro
+        MemWrite   => MemDataWE,                 -- Enable de escritura de la memoria de datos
+        Branch     => BranchCtrl,                -- Controla la escritura en memoria
+        ALUSrc     => ALUSrcCtrl,                -- Se activa cuando hay un salto condicional
+        RegDest    => RegDestCtrl,               -- Elige entre un registro y un dato inmediato como operando de la ALU
+        RegWrite   => RegWriteCtrl,              -- Enable de la escritura en los registros
+        RegToPC    => RegToPCCtrl,               -- Controla cual es el registro de destino
+        ExtCero    => ExtCeroCtrl,               -- 0 = dato inmediato extendido en signo, 1 = dato inmediato extendido en ceros
+        Jump       => JumpCtrl,                  -- Salto (jal, j)
+        PCToReg    => PCToRegCtrl                -- ProgramCounter => Registros (jal)
     );
 
     -- Instancia de la ALU
     Inst_ALUMIPS: ALUMIPS PORT MAP(
-        Op1        => Rd1,
-        Op2        => ALUSrcMux32Sal,
-        ALUControl => ALUControlCtrl,
-        Res        => ALUResSal,
-        Z          => ALUZSal
+        Op1        => Rd1,                       -- Operando 1 <= Rd1
+        Op2        => ALUSrcMux32Sal,            -- Operando 2 <= dato inmediato o Rd2
+        ALUControl => ALUControlCtrl,            -- Señad de la unidad de control
+        Res        => ALUResSal,                 -- Salida resultado
+        Z          => ALUZSal                    -- Salida z
     );
 
     -- Instancia del Banco de Registros
     Inst_RegsMIPS: RegsMIPS PORT MAP(
-        Clk  => Clk,
-        NRst => NRst,
-        A1   => MemProgData(25 downto 21),
-        A2   => MemProgData(20 downto 16),
-        A3   => PCToRegMux5Sal,
-        Wd3  => PCToRegMux32Sal,
-        We3  => RegWriteCtrl,
-        Rd1  => Rd1,
-        Rd2  => Rd2
+        Clk  => Clk,                             -- Reloj
+        NRst => NRst,                            -- Reset activo en bajo
+        A1   => MemProgData(25 downto 21),       -- Direccion del registro de lectura 1
+        A2   => MemProgData(20 downto 16),       -- Dirrecion del registro de Lectura 1
+        A3   => PCToRegMux5Sal,                  -- Direccion del registro de escritura
+        Wd3  => PCToRegMux32Sal,                 -- Dato a escribir en el registro
+        We3  => RegWriteCtrl,                    -- Write enable
+        Rd1  => Rd1,                             -- Salida dato registro de lectura 1
+        Rd2  => Rd2                              -- Salida dato registro de Lectura 2
     );
 
     -- Insancia del extesor de ceros
     Inst_ExtCero: ExtCero PORT MAP(
-        Ent  => MemProgData(15 downto 0),
-        Sal  => ExtCeroSal
+        Ent  => MemProgData(15 downto 0),        -- Entrada los 16 LSB de la instruccion
+        Sal  => ExtCeroSal                       -- Salida  = entrada y 16 ceros delante
     );
 
     -- Instancia del extesor de signo
     Inst_ExtSign: ExtSign PORT MAP(
-        Ent  => MemProgData(15 downto 0),
-        Sal  => ExtSignSal
+        Ent  => MemProgData(15 downto 0),        -- Entrada los 16 LSB de la instruccion
+        Sal  => ExtSignSal                       -- El número de la entrada extendido en signo
     );
 
-    -- Instancia del Program Counter
+    -- Instancia del Program Counter: Registro de 32 bits
     Inst_ProgCount: ProgCount PORT MAP(
-        Q    => ProgCountSal,
-        D    => RegToPCMux32Sal,
-        Clk  => Clk,
-        NRst => NRst
+        Q    => ProgCountSal,                    -- Entrada del registro
+        D    => RegToPCMux32Sal,                 -- Salida del registro
+        Clk  => Clk,                             -- Reloj
+        NRst => NRst                             -- Reset activo en bajo
     );
 
     -- Instancia del Sumador + 4
     Inst_Suma4: Suma4 PORT MAP(
-        D   => ProgCountSal,
-        Q   => Suma4Sal
+        D   => ProgCountSal,                     -- Entrada = Salida del ProgramCounter
+        Q   => Suma4Sal                          -- Salida = Entrada + 4
     );
 
     -- Instancia Mux32 - Cuando la señal de control está activa (instruccion beq) al ProgramCounter le llega el BTA
     Inst_PCSrcMux32: Mux32 PORT MAP(
-        D0  => Suma4Sal,
-        D1  => PCSrcMux32D1,
-        Sel => PCSrcCtrl,
-        Z   =>  PCSrcMux32Sal
+        D0  => Suma4Sal,                         -- DO = Salida_Suma4 = PC + 4
+        D1  => PCSrcMux32D1,                     -- D1 = BranchTarget
+        Sel => PCSrcCtrl,                        -- Señal de la unidad de control
+        Z   =>  PCSrcMux32Sal                    -- Salida del multiplexor
     );
 
-    -- Instancia Mux32 - Cuando la señal de control está activa (instruccion jump) al ProgramCounter le llega el JumpTarget
+    -- Instancia Mux32 - Cuando la señal de control está activa (instruccion jump) al ProgramCounter le llega el JumpTargetH
     Inst_JumpMux32: Mux32 PORT MAP(
-        D0  => PCSrcMux32Sal,
-        D1  => JumpMux32D1,
-        Sel => JumpCtrl,
-        Z   => JumpMux32Sal
+        D0  => PCSrcMux32Sal,                    -- Salida_Suma4 o BranchTarget
+        D1  => JumpMux32D1,                      -- JumpTarget
+        Sel => JumpCtrl,                         -- Señal de la undiad de control
+        Z   => JumpMux32Sal                      -- Salida del multiplexor
     );
 
     -- Instancia Mux32 - Cuando la señal de control está a '1' RD1 se conecta el Program Counter (en instrucciones jr)
     Inst_RegToPcMux32: Mux32 PORT MAP(
-        D0  => JumpMux32Sal,
-        D1  => Rd1,
-        Sel => RegToPCCtrl,
-        Z   => RegToPcMux32Sal
+        D0  => JumpMux32Sal,                     -- JumpTarget o Salida_Suma4 o BranchTarget
+        D1  => Rd1,                              -- Salida del registro de lectura 1
+        Sel => RegToPCCtrl,                      -- Señal de control de la alu
+        Z   => RegToPcMux32Sal                   -- Salida del multiplexor
     );
 
     -- Instancia Mux32 - Elige como opedando de la ALU entre la salida del extensor de signo o la salida del extensor de ceros
     Inst_ExtCeroMux32: Mux32 PORT MAP(
-        D0  => ExtSignSal,
-        D1  => ExtCeroSal,
-        Sel => ExtCeroCtrl,
-        Z   => ExtCeroMux32Sal
+        D0  => ExtSignSal,                       -- Salida del extensor se signo
+        D1  => ExtCeroSal,                       -- Salida del extensor de ceros
+        Sel => ExtCeroCtrl,                      -- Señal de la unidad de control
+        Z   => ExtCeroMux32Sal                   -- Salida del multiplexor
     );
 
     -- Instancia Mux32 - Elije como segundo opedando para la ALU entre la salida RD2 del registro o un dato inmediato
     Inst_ALUSrcMux32: Mux32 PORT MAP(
-        D0  => Rd2,
-        D1  => ExtCeroMux32Sal,
-        Sel => ALUSrcCtrl,
-        Z   => ALUSrcMux32Sal
+        D0  => Rd2,                              -- Segundo registro leido del banco de registro
+        D1  => ExtCeroMux32Sal,                  -- Dato extenido en signo o en ceros
+        Sel => ALUSrcCtrl,                       -- Señal de la unidad de control
+        Z   => ALUSrcMux32Sal                    -- Salida del multiplexor
     );
 
+    -- Instancia Mux32 - Elije como dato para escribir  en los registros emtre el resultado de la ALU y la salida de memoria de datos
     Inst_MemToRegMux32: Mux32 PORT MAP(
-        D0  => ALUResSal,
-        D1  => MemDataDataRead,
-        Sel => MemToRegCtrl,
-        Z   => MemToRegMux32Sal
+        D0  => ALUResSal,                        -- Resultado de la ALU
+        D1  => MemDataDataRead,                  -- Dato de la memoria
+        Sel => MemToRegCtrl,                     -- Señal de control de la ALU
+        Z   => MemToRegMux32Sal                  -- Salida del multiplexor
     );
 
+    -- Instancia Mux32 - Cuando está activo, lleva el Program Counter a los registros
     Inst_PCToRegMux32: Mux32 PORT MAP(
-        D0  => MemToRegMux32Sal,
-        D1  => Suma4Sal,
-        Sel => PCToRegCtrl,
-        Z   => PCToRegMux32Sal
+        D0  => MemToRegMux32Sal,                 -- Dato de la memoria o el resultado de la ALU
+        D1  => Suma4Sal,                         -- Salida_Suma4
+        Sel => PCToRegCtrl,                      -- Señal de control de la ALU
+        Z   => PCToRegMux32Sal                   -- Salida del multiplexor
     );
 
     -- Instancia Mux5 Elije como direccion del registro de destino los bits(20-16) o los bits(15-11) de la instruccion
     Inst_RegDestMux5: Mux5 PORT MAP(
-        D0  => MemProgData(20 downto 16),
-        D1  => MemProgData(15 downto 11),
-        Sel => RegDestCtrl,
-        Z   => RegDestMux5Sal
+        D0  => MemProgData(20 downto 16),        -- Los bits 20-16 de la instruccion
+        D1  => MemProgData(15 downto 11),        -- los bits 15-11 de la instruccion
+        Sel => RegDestCtrl,                      -- Señal de control de la ALU
+        Z   => RegDestMux5Sal                    -- Salida del multiplexor
         );
 
     -- Instancia Mux5 Elige como registro de destino entre la señal suministrda por el multiplexor RegDest y el registro 31
     Inst_PCToRegMux5: Mux5 PORT MAP(
-        D0  => RegDestMux5Sal,
-        D1  => "11111",
-        Sel => PCToRegCtrl,
-        Z   => PCToRegMux5Sal
+        D0  => RegDestMux5Sal,                   -- Los bits 20-16 o los bits 15-11 de la instruccion
+        D1  => "11111",                          -- 31
+        Sel => PCToRegCtrl,                      -- Señal de control de la ALU
+        Z   => PCToRegMux5Sal                    -- Salida del multiplexor
     );
 
 end Practica;
